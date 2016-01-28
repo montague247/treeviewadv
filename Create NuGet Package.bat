c476cad39a701fc96734285497d5272ee0b3a362
montague247 <montague247@users.sourcesorge.net>
2016-01-28 01:09:47 +0100

    Added build system

---
+++ Create NuGet Package.bat
@@ -0,0 +1,13 @@
+:: search for msbuild and make it available
+set bb.build.msbuild.exe=
+for /D %%D in (%SYSTEMROOT%\Microsoft.NET\Framework\v4*) do set msbuildPath=%%D
+set PATH=%PATH%;%msbuildPath%
+
+:: make sure we have a clean release build
+msbuild /t:Rebuild /p:Configuration=Release /p:Platform="Any CPU" TreeViewAdv.sln
+
+:: remove any existing nupkg files
+del *.nupkg
+
+:: build the nuget packages
+.nuget\nuget pack Aga.Controls\Aga.Controls.csproj -Properties Configuration=Release
